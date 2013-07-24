1ae571f17231f9ceb3376bd9be10108577298816
Jastamo <bjorn.soderberg97@gmail.com>
2013-07-24 13:48:12 +0200

    Moved word generation to MySQL database
    Instead of being generated by the application, the word is now generated
    from a MySQL database with PHP.

--- Game/Game Source Code/src/com/gamesourcecode/game/GameActivity.java
+++ Game/Game Source Code/src/com/gamesourcecode/game/GameActivity.java
@@ -1,9 +1,20 @@
 package com.gamesourcecode.game;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
-import java.util.Random;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
@@ -11,36 +22,39 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+
+import com.gamesourcecode.misc.JSONParser;
 
 public class GameActivity extends Activity {
 
 	private String word = "";
-	
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// Makes the game go fullscreen		
+		// Makes the game go fullscreen
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		
-		word = getRandomWord();
-		
+
+		// word = getRandomWord();
+
 		LoadBitmaps loader = new LoadBitmaps();
 		loader.execute(this);
 
 	}
-	
+
 	// Just for testing to swap between images
-	private String getRandomWord() {
-		Random random = new Random();
-		int i = random.nextInt(3);
-		if(i == 0) return "guitar";
-		if(i == 1) return "cat";
-		if(i == 2) return "sydney";
-		return "";
-	}
+	// private String getRandomWord() {
+	// Random random = new Random();
+	// int i = random.nextInt(3);
+	// if(i == 0) return "guitar";
+	// if(i == 1) return "cat";
+	// if(i == 2) return "sydney";
+	// return "";
+	// }
 
 	public int getWidth() {
 		DisplayMetrics dm = new DisplayMetrics();
@@ -57,14 +71,16 @@ public class GameActivity extends Activity {
 	private class LoadBitmaps extends AsyncTask<GameActivity, Integer, Bitmap[]> {
 
 		private GameActivity activity;
+		private JSONParser jsonParser = new JSONParser();
 
 		protected void onPreExecute() {
 			super.onPreExecute();
-			GameActivity.this.setProgressBarIndeterminateVisibility(true);
 		}
 
 		protected Bitmap[] doInBackground(GameActivity... params) {
 			activity = params[0];
+
+			word = getWord();
 
 			Bitmap[] bitmaps = new Bitmap[24];
 
@@ -81,8 +97,6 @@ public class GameActivity extends Activity {
 			Game game = new Game(activity, activity, result, word);
 
 			activity.setContentView(game);
-
-			GameActivity.this.setProgressBarIndeterminateVisibility(false);
 
 		}
 
@@ -102,5 +116,76 @@ public class GameActivity extends Activity {
 
 			return null;
 		}
+
+		String url = "http://192.168.60.49/MySQL/getimage.php";
+
+		private String getWord() {
+			String result = "";
+			InputStream is = null;
+			
+			try {
+				
+				HttpClient client = new DefaultHttpClient();
+				HttpPost post = new HttpPost(url);
+				HttpResponse response = client.execute(post);
+				HttpEntity entity = response.getEntity();
+				is = entity.getContent();
+				
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			try {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+				StringBuilder sb = new StringBuilder();
+				String line = null;
+				while((line = reader.readLine()) != null) {
+					sb.append(line + "\n");
+				}
+				
+				is.close();
+				result = sb.toString();
+			} catch (Exception e) {
+				
+			}
+			
+			try {
+				JSONArray jArray = new JSONArray(result);
+				for(int i = 0; i < jArray.length(); i++) {
+					JSONObject jData = jArray.getJSONObject(i);
+					Log.i("test", jData.getString("name"));
+					result = jData.getString("name");
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			
+			
+			
+			
+
+			return result;
+		}
+		// private JSONObject getJSONObject() throws ClientProtocolException,
+		// IOException, JSONException {
+		// StringBuilder sb = new StringBuilder(url);
+		// HttpClient client = new DefaultHttpClient();
+		//
+		// HttpGet get = new HttpGet(sb.toString());
+		// HttpResponse response = client.execute(get);
+		// int status = response.getStatusLine().getStatusCode();
+		// if (status == 200) {
+		// HttpEntity entity = response.getEntity();
+		// String data = EntityUtils.toString(entity);
+		// JSONArray jArray = new JSONArray(data);
+		// JSONObject jObject = jArray.getJSONObject(0);
+		// return jObject;
+		// } else {
+		// Toast.makeText(GameActivity.this, "Error", Toast.LENGTH_SHORT);
+		// return null;
+		// }
+		// }
 	}
 }
---
+++ Game/Networking/getimage.php
@@ -0,0 +1,42 @@
+<?php
+	// Connect to MySQL
+	$connect = mysql_connect("localhost", "root", "david") or die("Error connecting to database");
+	
+	// Select database
+	mysql_select_db("android");
+	
+	
+	$query = "
+		SELECT name FROM images
+	";
+	
+	$result = mysql_query($query);
+	
+	$length = mysql_num_rows($result);
+	
+	do {
+		// Random index in database
+		$index = rand(1, $length);
+		
+		$query = "
+			SELECT name FROM images WHERE id = $index
+		";
+		
+		$result = mysql_query($query);
+
+		while($row = mysql_fetch_array($result)) {
+			if(isset($row)) {
+				$output[] = $row;
+			}
+		}
+	} while(!isset($output));
+	
+	print(json_encode($output));
+	mysql_close();
+
+
+
+
+
+
+?>
